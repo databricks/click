9657fa3896891ec6e9806edf3744ea044c10cbae
Nick Lanham <nick@afternight.org>
2017-06-27 10:49:39 -0700

    Add apache license

--- src/certs.rs
+++ src/certs.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use der_parser;
 use regex::Regex;
 use rustls::{Certificate, PrivateKey};
--- src/error.rs
+++ src/error.rs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-
 use base64;
 use hyper;
 use serde_json;

